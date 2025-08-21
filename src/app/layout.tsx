d590cb6b5fd04c46dbdeae0769c1ddc5e8bc4c35
Jdamour12 <jeandamourkubwimana0@gmail.com>
2025-08-21 18:01:29 +0200

    Changed the title of the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SAMPS UR - Academic Portal",
+  title: "SAMPS UR",
   description: "Academic Portal for University of Rwanda",
 };
 
