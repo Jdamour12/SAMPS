9a485caf90f7a171666ec9ade2eb20c3a8027d7c
Jdamour12 <jeandamourkubwimana0@gmail.com>
2025-08-30 18:46:24 +0200

    updated the name of the app in json file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SAMPS UR",
+  title: "SAMPS UR - Lecturer Portal",
   description: "Academic Portal for University of Rwanda",
 };
 
